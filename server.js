5e7467140ecaece57d78dad7752ab2ba4fd28a8d
Georgesa98 <georgesalebe0@gmail.com>
2023-11-18 15:46:00 +0200

    .

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cookieParser = require("cookie-parser");
 const corsOptions = require("./config/corsOptions.js");
 const cors = require("cors");
 const serverless = require("serverless-http");
+const router = express.Router();
 const bodyParser = require("body-parser");
 const PORT = 3500;
 connectDB();
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // app.use("/", express.static(path.join(__dirname, "public"))); // Uncomment this if you want to use css files for html in "Views"
+app.use("/.netlify/functions/server", router);
 app.use(bodyParser.json());
 app.use("/", require("./routes/root.js"));
 app.use("/auth", require("./routes/authRoutes.js"));
