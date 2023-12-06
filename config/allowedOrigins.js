f2678fabfb66d7ee44505de86aa456fe2e8fa721
Georgesa98 <georgesalebe0@gmail.com>
2023-12-07 00:30:39 +0200

    added frontend url to cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:5173",
   "http://localhost:FRONT_END_PROT",
+  "https://todolist-gems2004.netlify.app",
 ];
 module.exports = allowedOrigins;
